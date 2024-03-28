1897f44d7f8d004e7e0bf6c51bf53854a044016c
lonelywh1te <lonelywh1te@ya.ru>
2024-03-28 15:56:58 +1000

    add: English localization

--- app/src/main/java/ru/lonelywh1te/kotlin_tasklist/presentation/view/InfoFragment.kt
+++ app/src/main/java/ru/lonelywh1te/kotlin_tasklist/presentation/view/InfoFragment.kt
@@ -5,12 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import ru.lonelywh1te.kotlin_tasklist.BuildConfig
 import ru.lonelywh1te.kotlin_tasklist.databinding.FragmentInfoBinding
 
 class InfoFragment : Fragment() {
     private lateinit var binding: FragmentInfoBinding
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         binding = FragmentInfoBinding.inflate(layoutInflater, container, false)
+        binding.tvAppVersionInfo.text = BuildConfig.VERSION_NAME
         return binding.root
     }
 }
