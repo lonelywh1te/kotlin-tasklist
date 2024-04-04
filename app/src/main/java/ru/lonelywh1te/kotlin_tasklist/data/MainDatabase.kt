8f578f24f8b1ce7961355a510f8739e0acee5deb
lonelywh1te <lonelywh1te@ya.ru>
2024-04-04 11:31:58 +1000

    add: Database migration from 1 to 2

--- app/src/main/java/ru/lonelywh1te/kotlin_tasklist/data/MainDatabase.kt
+++ app/src/main/java/ru/lonelywh1te/kotlin_tasklist/data/MainDatabase.kt
@@ -4,12 +4,20 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import ru.lonelywh1te.kotlin_tasklist.data.dao.TaskDao
 import ru.lonelywh1te.kotlin_tasklist.data.dao.TaskGroupDao
 import ru.lonelywh1te.kotlin_tasklist.data.entity.Task
 import ru.lonelywh1te.kotlin_tasklist.data.entity.TaskGroup
 
-@Database(entities = [Task::class, TaskGroup::class], version = 1)
+val MIGRATION_1_2: Migration = object: Migration(1, 2) {
+    override fun migrate(database: SupportSQLiteDatabase) {
+        database.execSQL("ALTER TABLE task_table ADD COLUMN completionDateInMillis BIGINT DEFAULT NULL")
+    }
+}
+
+@Database(entities = [Task::class, TaskGroup::class], version = 2)
 abstract class MainDatabase: RoomDatabase() {
     abstract fun TaskDao(): TaskDao
     abstract fun TaskGroupDao(): TaskGroupDao
@@ -20,7 +28,9 @@ abstract class MainDatabase: RoomDatabase() {
         fun getDatabase(context: Context): MainDatabase {
             if (db == null) {
                 synchronized(MainDatabase::class) {
-                    db = Room.databaseBuilder(context.applicationContext, MainDatabase::class.java, "tasks_db").build()
+                    db = Room.databaseBuilder(context.applicationContext, MainDatabase::class.java, "tasks_db")
+                        .addMigrations(MIGRATION_1_2)
+                        .build()
                 }
             }
 
