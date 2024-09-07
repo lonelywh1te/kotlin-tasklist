d9373b0c83d9d6479eed8ac33c695e9230ba3053
lonelywh1te <lonelywh1te@ya.ru>
2024-09-07 12:26:35 +1000

    fix: TaskItem's reordering

--- app/src/main/java/ru/lonelywh1te/kotlin_tasklist/domain/usecase/taskUseCases/MoveTaskToTaskGroupUseCase.kt
+++ app/src/main/java/ru/lonelywh1te/kotlin_tasklist/domain/usecase/taskUseCases/MoveTaskToTaskGroupUseCase.kt
@@ -5,14 +5,23 @@ import ru.lonelywh1te.kotlin_tasklist.domain.repository.TaskRepository
 
 class MoveTaskToTaskGroupUseCase(private val taskRepository: TaskRepository) {
     suspend fun execute(task: Task, taskGroupId: Int?) {
-        taskRepository.updateTask(task.copy(taskGroupId = taskGroupId, order = getOrder(taskGroupId)))
+        taskRepository.updateTask(task.copy(taskGroupId = taskGroupId, order = getTasks(taskGroupId).size))
+        reorderTasksFrom(task)
     }
 
-    private suspend fun getOrder(taskGroupId: Int?): Int {
+    private suspend fun reorderTasksFrom(task: Task) {
+        val tasks = getTasks(task.taskGroupId)
+
+        tasks.filter { it.order > task.order }.forEach {
+            taskRepository.updateTask(it.copy(order = it.order - 1))
+        }
+    }
+
+    private suspend fun getTasks(taskGroupId: Int?): List<Task> {
         return if (taskGroupId == null) {
-            taskRepository.getAllTasks().size
+            taskRepository.getAllTasks()
         } else {
-            taskRepository.getAllTasksFromGroup(taskGroupId).size
+            taskRepository.getAllTasksFromGroup(taskGroupId)
         }
     }
 }
